ad42d8fcc303a982fbe58a728c9c2c4e35fa28cb
LeeHuSung <mn040820@naver.com>
2024-05-24 15:59:40 +0900

    .

--- src/main/java/com/patientpal/backend/common/BaseEntity.java
+++ src/main/java/com/patientpal/backend/common/BaseEntity.java
@@ -5,28 +5,17 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
 import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
 @EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity {
-
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdDate;
+public abstract class BaseEntity extends BaseTimeEntity {
 
     @CreatedBy
     @Column(updatable = false)
     private String createdBy;
-
-    @LastModifiedDate
-    private LocalDateTime lastModifiedDate;
 
     @LastModifiedBy
     private String lastModifiedBy;
