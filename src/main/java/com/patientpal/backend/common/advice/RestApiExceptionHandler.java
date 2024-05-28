50a3b9f35bd8a60273340ff1b803a486f3471cf0
destitutor <binarybard@pm.me>
2024-05-28 15:02:51 +0900

    feat: REST API 호출 시 발생하는 범용 예외 전용 핸들러 추가

---
+++ src/main/java/com/patientpal/backend/common/advice/RestApiExceptionHandler.java
@@ -0,0 +1,51 @@
+package com.patientpal.backend.common.advice;
+
+import com.patientpal.backend.common.exception.BusinessException;
+import com.patientpal.backend.common.exception.ErrorCode;
+import com.patientpal.backend.common.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@Slf4j
+@RestControllerAdvice
+public class RestApiExceptionHandler {
+    @ExceptionHandler(AuthenticationException.class)
+    protected ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
+        var response = ErrorResponse.of(ErrorCode.AUTHENTICATION_FAILED);
+        log.debug("Authentication failed: {}", e.getMessage());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        var response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE);
+        log.debug("Parameter type is invalid: {}", e.getMessage());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        log.debug("Argument validation has failed: {}", e.getMessage());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        var response = ErrorResponse.of(e.getErrorCode());
+        log.error("Unexpected error has occurred: {}", e.getDetail());
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        var response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        log.error(e.getMessage(), e);
+        return new ResponseEntity<>(response, response.getStatus());
+    }
+}
