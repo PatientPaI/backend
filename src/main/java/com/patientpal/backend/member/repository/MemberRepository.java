96a154e1b38b93134f9465a66fb6c0a97962ef57
destitutor <binarybard@pm.me>
2024-05-28 14:56:41 +0900

    feat: 회원가입 로직을 담당하는 서비스 계층 추가

---
+++ src/main/java/com/patientpal/backend/member/repository/MemberRepository.java
@@ -0,0 +1,20 @@
+package com.patientpal.backend.member.repository;
+
+import com.patientpal.backend.common.exception.EntityNotFoundException;
+import com.patientpal.backend.common.exception.ErrorCode;
+import com.patientpal.backend.member.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+
+    default Member findByUsernameOrThrow(String username) {
+        return findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_EXIST, username));
+    }
+
+    void deleteByUsername(String username);
+}
---
+++ src/main/java/com/patientpal/backend/member/service/MemberService.java
@@ -0,0 +1,42 @@
+package com.patientpal.backend.member.service;
+
+import com.patientpal.backend.auth.dto.SignUpRequest;
+import com.patientpal.backend.common.exception.AuthenticationException;
+import com.patientpal.backend.common.exception.ErrorCode;
+import com.patientpal.backend.member.domain.Member;
+import com.patientpal.backend.member.dto.MemberResponse;
+import com.patientpal.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Long save(SignUpRequest request) {
+        try {
+            var member = SignUpRequest.of(request);
+            member.encodePassword(passwordEncoder);
+            return memberRepository.save(member).getId();
+        } catch (DataIntegrityViolationException e) {
+            throw new AuthenticationException(ErrorCode.MEMBER_ALREADY_EXIST, request.getUsername());
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponse findByUsername(String username) {
+        Member foundMember = memberRepository.findByUsernameOrThrow(username);
+        return MemberResponse.of(foundMember);
+    }
+
+    public void deleteByUsername(String username) {
+        memberRepository.deleteByUsername(username);
+    }
+}
