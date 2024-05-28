2954ad3e420a73ca39abb3604e54b5a20f362762
wjdwwidz <wjdwwidz@gmail.com>
2024-05-29 00:20:28 +0900

    Refactor: BaseTimeEntity 적용

--- src/main/java/com/patientpal/backend/post/domain/Post.java
+++ src/main/java/com/patientpal/backend/post/domain/Post.java
@@ -1,21 +1,18 @@
 package com.patientpal.backend.post.domain;
 
+import com.patientpal.backend.common.BaseTimeEntity;
 import com.patientpal.backend.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Table(name = "posts")
 @RequiredArgsConstructor
-public class Post {
+public class Post extends BaseTimeEntity {
 
     @Id
     @Column(name = "post_id")
@@ -31,14 +28,6 @@ public class Post {
 
     @Column(nullable = false)
     private String content;
-
-    @Column(nullable = false, insertable = false)
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @Column(nullable = false)
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
 
     @Enumerated(EnumType.STRING)
     private PostType postType;
