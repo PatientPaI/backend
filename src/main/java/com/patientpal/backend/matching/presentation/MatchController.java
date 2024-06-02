b7a9160a5d2edcb28c366878139010a3bc79c3cc
LeeHuSung <mn040820@naver.com>
2024-06-02 14:52:52 +0900

    Feat: MatchController에 Swagger 구현

--- src/main/java/com/patientpal/backend/matching/presentation/MatchController.java
+++ src/main/java/com/patientpal/backend/matching/presentation/MatchController.java
@@ -1,10 +1,14 @@
 package com.patientpal.backend.matching.presentation;
 
+import com.patientpal.backend.common.exception.ErrorResponse;
 import com.patientpal.backend.matching.application.MatchService;
-
 import com.patientpal.backend.matching.dto.response.MatchListResponse;
 import com.patientpal.backend.matching.dto.response.MatchResponse;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.*;
 
+@Tag(name = "MATCH API", description = "매칭 관련 API")
 @RestController
 @RequestMapping("api/v1/matches")
 @RequiredArgsConstructor
@@ -21,32 +26,66 @@ public class MatchController {
 
     private final MatchService matchService;
 
-    /**
-     * TODO
-     *  - createMatch()시, createdBy 에 null 들어가는 문제 ->
-     *  아니면 멤버를 통해 환자, 간병인으로 나뉘고 매치가 등록돼서 누구로 등록해야할지 모르나?
-     *  - Matching 신청 할 때, 프로필을 등록했는지 확인하는 기능 필수. 현재 Patient, Caregiver 엔티티는 회원 가입 시, ID만 가진 빈껍데기로 생성이 됨.
-     *  그 안에 세부 내용들을 채워넣어야 매칭 신청, 등록 가능. -> 프로필 세부 내용을 작성해야 매칭 신청 or 매칭 리스트에 등록 가능하게 해야함.
-     */
-
+    @Operation(
+            summary = "매칭 생성 for 환자",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "매칭 생성 성공",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MatchResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PostMapping("/patient/{responseMemberId}")
     public ResponseEntity<MatchResponse> createMatchForPatient(@AuthenticationPrincipal User currentMember,
-                                               @PathVariable Long responseMemberId) {
+                                                               @PathVariable Long responseMemberId) {
         return ResponseEntity.status(CREATED).body(matchService.createForPatient(currentMember, responseMemberId));
     }
 
+    @Operation(
+            summary = "매칭 생성 for 간병인",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "매칭 생성 성공",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MatchResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PostMapping("/caregiver/{responseMemberId}")
     public ResponseEntity<MatchResponse> createMatchForCaregiver(@AuthenticationPrincipal User currentMember,
-                                                 @PathVariable Long responseMemberId) {
+                                                                 @PathVariable Long responseMemberId) {
         return ResponseEntity.status(CREATED).body(matchService.createForCaregiver(currentMember, responseMemberId));
     }
 
+    @Operation(
+            summary = "매칭 정보 조회",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "매칭 조회 성공",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MatchResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "매칭 정보 없음",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @GetMapping("/{matchId}")
     public ResponseEntity<MatchResponse> getMatch(@AuthenticationPrincipal User currentMember, @PathVariable Long matchId) {
         final MatchResponse match = matchService.getMatch(matchId, currentMember.getUsername());
         return ResponseEntity.status(OK).body(match);
     }
 
+    @Operation(
+            summary = "매칭 리스트 조회",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "매칭 리스트 조회 성공",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = MatchListResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @GetMapping("/all")
     public ResponseEntity<MatchListResponse> getMatchList(@AuthenticationPrincipal User currentMember,
                                                           @RequestParam(defaultValue = "0") int page,
@@ -55,12 +94,32 @@ public class MatchController {
         return ResponseEntity.status(OK).body(matchList);
     }
 
+    @Operation(
+            summary = "매칭 취소",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "매칭 취소 성공"),
+                    @ApiResponse(responseCode = "404", description = "매칭 정보 없음",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PostMapping("/{matchId}/cancel")
     public ResponseEntity<Void> cancelMatch(@PathVariable Long matchId, @AuthenticationPrincipal User currentMember) {
         matchService.cancelMatch(matchId, currentMember.getUsername());
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+            summary = "매칭 수락",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "매칭 수락 성공"),
+                    @ApiResponse(responseCode = "404", description = "매칭 정보 없음",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "401", description = "인증 실패",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
     @PostMapping("{matchId}/accept")
     public ResponseEntity<Void> acceptMatch(@PathVariable Long matchId, @AuthenticationPrincipal User currentMember) {
         matchService.acceptMatch(matchId, currentMember.getUsername());
