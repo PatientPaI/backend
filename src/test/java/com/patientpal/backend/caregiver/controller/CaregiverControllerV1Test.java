3d8e8d6e15b25dce8a568f31fc6fbf8b77e10974
LeeHuSung <mn040820@naver.com>
2024-06-10 15:26:24 +0900

    Test: 프로필 Controller Test 리팩토링 - CommonControllerSliceTest 상속으로 인한 슬라이스 테스트 & 중복 제거 - 내부에서 클래스 분리

--- src/test/java/com/patientpal/backend/caregiver/controller/CaregiverControllerV1Test.java
+++ src/test/java/com/patientpal/backend/caregiver/controller/CaregiverControllerV1Test.java
@@ -15,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.patientpal.backend.caregiver.dto.request.CaregiverProfileCreateRequest;
 import com.patientpal.backend.caregiver.dto.request.CaregiverProfileUpdateRequest;
 import com.patientpal.backend.caregiver.dto.response.CaregiverProfileResponse;
@@ -23,167 +22,157 @@ import com.patientpal.backend.caregiver.service.CaregiverService;
 import com.patientpal.backend.common.custommockuser.WithCustomMockUserCaregiver;
 import com.patientpal.backend.common.exception.EntityNotFoundException;
 import com.patientpal.backend.common.exception.ErrorCode;
-import com.patientpal.backend.member.service.MemberService;
-import com.patientpal.backend.security.jwt.JwtTokenProvider;
-import org.junit.jupiter.api.BeforeEach;
+import com.patientpal.backend.test.CommonControllerSliceTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest(CaregiverControllerV1.class)
-public class CaregiverControllerV1Test {
-
-    @MockBean
-    private CaregiverService caregiverService;
-
-    @MockBean
-    private MemberService memberService;
-
-    @InjectMocks
-    private CaregiverControllerV1 caregiverController;
+@SuppressWarnings("NonAsciiCharacters")
+class CaregiverControllerV1Test extends CommonControllerSliceTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    CaregiverService caregiverService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    @Nested
+    class 간병인_프로필_생성 {
 
-    @MockBean
-    private JwtTokenProvider jwtTokenProvider;
+        @Test
+        @DisplayName("간병인 프로필을 성공적으로 생성한다.")
+        @WithCustomMockUserCaregiver
+        void successCreateCaregiverProfile() throws Exception {
+            // given
+            CaregiverProfileCreateRequest request = createCaregiverProfileRequest();
+            CaregiverProfileResponse response = createCaregiverProfileResponse();
+            given(caregiverService.saveCaregiverProfile(any(String.class), any(CaregiverProfileCreateRequest.class))).willReturn(response);
 
-    @BeforeEach
-    void setUp(WebApplicationContext context) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+            // when & then
+            mockMvc.perform(post("/api/v1/caregiver")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name").value(response.getName()))
+                    .andExpect(jsonPath("$.residentRegistrationNumber").value(response.getResidentRegistrationNumber()))
+                    .andExpect(jsonPath("$.phoneNumber").value(response.getPhoneNumber()))
+                    .andExpect(jsonPath("$.address.street").value(response.getAddress().getStreet()));
+        }
+
+        @Test
+        @DisplayName("간병인 프로필을 생성할 때 잘못된 요청이면 예외가 발생한다.")
+        @WithCustomMockUserCaregiver
+        void failCreateCaregiverProfileBadRequest() throws Exception {
+            // given
+            CaregiverProfileCreateRequest request = new CaregiverProfileCreateRequest("", "", "", null, 0, 1, "", "");
+
+            // when & then
+            mockMvc.perform(post("/api/v1/caregiver")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
     }
 
-    @Test
-    @DisplayName("간병인 프로필을 성공적으로 생성한다.")
-    @WithCustomMockUserCaregiver
-    void successCreateCaregiverProfile() throws Exception {
-        // given
-        CaregiverProfileCreateRequest request = createCaregiverProfileRequest();
-        CaregiverProfileResponse response = createCaregiverProfileResponse();
-        given(caregiverService.saveCaregiverProfile(any(String.class), any(CaregiverProfileCreateRequest.class))).willReturn(response);
+    @Nested
+    class 간병인_프로필_조회 {
 
-        // when & then
-        mockMvc.perform(post("/api/v1/caregiver")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-                        .characterEncoding("UTF-8")
-                )
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.name").value(response.getName()))
-                .andExpect(jsonPath("$.residentRegistrationNumber").value(response.getResidentRegistrationNumber()))
-                .andExpect(jsonPath("$.phoneNumber").value(response.getPhoneNumber()))
-                .andExpect(jsonPath("$.address.street").value(response.getAddress().getStreet()));
+        @Test
+        @DisplayName("간병인 프로필을 성공적으로 조회한다.")
+        @WithCustomMockUserCaregiver
+        void successGetCaregiverProfile() throws Exception {
+            // given
+            CaregiverProfileResponse response = createCaregiverProfileResponse();
+            given(caregiverService.getProfile(any(String.class))).willReturn(response);
+
+            // when & then
+            mockMvc.perform(get("/api/v1/caregiver"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value(response.getName()))
+                    .andExpect(jsonPath("$.residentRegistrationNumber").value(response.getResidentRegistrationNumber()))
+                    .andExpect(jsonPath("$.phoneNumber").value(response.getPhoneNumber()))
+                    .andExpect(jsonPath("$.address.street").value(response.getAddress().getStreet()));
+        }
+
+        @Test
+        @DisplayName("간병인 프로필을 조회할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserCaregiver
+        void failGetCaregiverProfileNotFound() throws Exception {
+            // given
+            given(caregiverService.getProfile(any(String.class))).willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST));
+
+            // when & then
+            mockMvc.perform(get("/api/v1/caregiver"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 
-    @Test
-    @DisplayName("간병인 프로필을 성공적으로 조회한다.")
-    @WithCustomMockUserCaregiver
-    void successGetCaregiverProfile() throws Exception {
-        // given
-        CaregiverProfileResponse response = createCaregiverProfileResponse();
-        given(caregiverService.getProfile(any(String.class))).willReturn(response);
+    @Nested
+    class 간병인_프로필_수정 {
 
-        // when & then
-        mockMvc.perform(get("/api/v1/caregiver"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        @Test
+        @DisplayName("간병인 프로필을 성공적으로 수정한다.")
+        @WithCustomMockUserCaregiver
+        void successUpdateCaregiverProfile() throws Exception {
+            // given
+            CaregiverProfileUpdateRequest request = updateCaregiverProfileRequest();
+            willDoNothing().given(caregiverService).updateCaregiverProfile(any(String.class), any(CaregiverProfileUpdateRequest.class));
+
+            // when & then
+            mockMvc.perform(patch("/api/v1/caregiver")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isNoContent());
+        }
+
+        @Test
+        @DisplayName("간병인 프로필을 수정할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserCaregiver
+        void failUpdateCaregiverProfileNotFound() throws Exception {
+            // given
+            CaregiverProfileUpdateRequest request = updateCaregiverProfileRequest();
+            willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST)).given(caregiverService).updateCaregiverProfile(any(String.class), any(CaregiverProfileUpdateRequest.class));
+
+            // when & then
+            mockMvc.perform(patch("/api/v1/caregiver")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 
-    // 기능 검증: 프로필 수정
-    @Test
-    @DisplayName("간병인 프로필을 성공적으로 수정한다.")
-    @WithCustomMockUserCaregiver
-    void successUpdateCaregiverProfile() throws Exception {
-        // given
-        CaregiverProfileUpdateRequest request = updateCaregiverProfileRequest();
-        willDoNothing().given(caregiverService).updateCaregiverProfile(any(String.class), any(CaregiverProfileUpdateRequest.class));
+    @Nested
+    class 간병인_프로필_삭제 {
 
-        // when & then
-        mockMvc.perform(patch("/api/v1/caregiver")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-    }
+        @Test
+        @DisplayName("간병인 프로필을 성공적으로 삭제한다.")
+        @WithCustomMockUserCaregiver
+        void successDeleteCaregiverProfile() throws Exception {
+            // given
+            willDoNothing().given(caregiverService).deleteCaregiverProfile(any(String.class));
 
-    @Test
-    @DisplayName("간병인 프로필을 성공적으로 삭제한다.")
-    @WithCustomMockUserCaregiver
-    void successDeleteCaregiverProfile() throws Exception {
-        // given
-        willDoNothing().given(caregiverService).deleteCaregiverProfile(any(String.class));
+            // when & then
+            mockMvc.perform(delete("/api/v1/caregiver"))
+                    .andDo(print())
+                    .andExpect(status().isNoContent());
+        }
 
-        // when & then
-        mockMvc.perform(delete("/api/v1/caregiver"))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-    }
+        @Test
+        @DisplayName("간병인 프로필을 삭제할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserCaregiver
+        void failDeleteCaregiverProfileNotFound() throws Exception {
+            // given
+            willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST)).given(caregiverService).deleteCaregiverProfile(any(String.class));
 
-    @Test
-    @DisplayName("간병인 프로필을 등록할 때 잘못된 요청이면 예외가 발생한다.")
-    @WithCustomMockUserCaregiver
-    void failCreateCaregiverProfileBadRequest() throws Exception {
-        // given
-        CaregiverProfileCreateRequest request = new CaregiverProfileCreateRequest("", "", "", null, 0, 1, "", "");
-
-        // when & then
-        mockMvc.perform(post("/api/v1/caregiver")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("간병인 프로필을 조회할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserCaregiver
-    void failGetCaregiverProfileNotFound() throws Exception {
-        // given
-        given(caregiverService.getProfile(any(String.class))).willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST));
-
-        // when & then
-        mockMvc.perform(get("/api/v1/caregiver"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("간병인 프로필을 수정할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserCaregiver
-    void failUpdateCaregiverProfileNotFound() throws Exception {
-        // given
-        CaregiverProfileUpdateRequest request = updateCaregiverProfileRequest();
-        willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST)).given(caregiverService).updateCaregiverProfile(any(String.class), any(CaregiverProfileUpdateRequest.class));
-
-        // when & then
-        mockMvc.perform(patch("/api/v1/caregiver")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("간병인 프로필을 삭제할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserCaregiver
-    void failDeleteCaregiverProfileNotFound() throws Exception {
-        // given
-        willThrow(new EntityNotFoundException(ErrorCode.CAREGIVER_NOT_EXIST)).given(caregiverService).deleteCaregiverProfile(any(String.class));
-
-        // when & then
-        mockMvc.perform(delete("/api/v1/caregiver"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
+            // when & then
+            mockMvc.perform(delete("/api/v1/caregiver"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 }
--- src/test/java/com/patientpal/backend/patient/controller/PatientControllerV1Test.java
+++ src/test/java/com/patientpal/backend/patient/controller/PatientControllerV1Test.java
@@ -12,171 +12,167 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.patientpal.backend.common.custommockuser.WithCustomMockUserPatient;
 import com.patientpal.backend.common.exception.EntityNotFoundException;
 import com.patientpal.backend.common.exception.ErrorCode;
-import com.patientpal.backend.member.service.MemberService;
 import com.patientpal.backend.patient.dto.request.PatientProfileCreateRequest;
 import com.patientpal.backend.patient.dto.request.PatientProfileUpdateRequest;
 import com.patientpal.backend.patient.dto.response.PatientProfileResponse;
 import com.patientpal.backend.patient.service.PatientService;
-import com.patientpal.backend.security.jwt.JwtTokenProvider;
-import org.junit.jupiter.api.BeforeEach;
+import com.patientpal.backend.test.CommonControllerSliceTest;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@WebMvcTest(PatientControllerV1.class)
-public class PatientControllerV1Test {
+@SuppressWarnings("NonAsciiCharacters")
+public class PatientControllerV1Test extends CommonControllerSliceTest {
 
-    @MockBean
+    @Autowired
     private PatientService patientService;
 
-    @MockBean
-    private MemberService memberService;
+    @Nested
+    class 환자_프로필_생성 {
 
-    @InjectMocks
-    private PatientControllerV1 patientController;
+        @Test
+        @DisplayName("환자 프로필을 성공적으로 생성한다.")
+        @WithCustomMockUserPatient
+        void successCreatePatientProfile() throws Exception {
+            // given
+            PatientProfileCreateRequest request = createPatientProfileRequest();
+            PatientProfileResponse response = createPatientProfileResponse();
+            given(patientService.savePatientProfile(any(String.class), any(PatientProfileCreateRequest.class))).willReturn(response);
 
-    @Autowired
-    private MockMvc mockMvc;
+            // when & then
+            mockMvc.perform(post("/api/v1/patient")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("$.name").value(response.getName()))
+                    .andExpect(jsonPath("$.residentRegistrationNumber").value(response.getResidentRegistrationNumber()))
+                    .andExpect(jsonPath("$.phoneNumber").value(response.getPhoneNumber()))
+                    .andExpect(jsonPath("$.address.street").value(response.getAddress().getStreet()));
+        }
 
-    @Autowired
-    private ObjectMapper objectMapper;
+        @Test
+        @DisplayName("환자 프로필을 생성할 때 잘못된 요청이면 예외가 발생한다.")
+        @WithCustomMockUserPatient
+        void failCreatePatientProfileBadRequest() throws Exception {
+            // given
+            PatientProfileCreateRequest request = new PatientProfileCreateRequest("", "", "", null, "", "", "", "");
 
-    @MockBean
-    private JwtTokenProvider jwtTokenProvider;
-
-    @BeforeEach
-    void setUp(WebApplicationContext context) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+            // when & then
+            mockMvc.perform(post("/api/v1/patient")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isBadRequest());
+        }
     }
 
-    @Test
-    @DisplayName("환자 프로필을 성공적으로 생성한다.")
-    @WithCustomMockUserPatient
-    void successCreatePatientProfile() throws Exception {
-        // given
-        PatientProfileCreateRequest request = createPatientProfileRequest();
-        PatientProfileResponse response = createPatientProfileResponse();
-        given(patientService.savePatientProfile(any(String.class), any(PatientProfileCreateRequest.class))).willReturn(response);
+    @Nested
+    class 환자_프로필_조회 {
 
-        // when & then
-        mockMvc.perform(post("/api/v1/patient")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-                )
-                .andDo(print())
-                .andExpect(status().isCreated());
+        @Test
+        @DisplayName("환자 프로필을 성공적으로 조회한다.")
+        @WithCustomMockUserPatient
+        void successGetPatientProfile() throws Exception {
+            // given
+            PatientProfileResponse response = createPatientProfileResponse();
+            given(patientService.getProfile(any(String.class))).willReturn(response);
+
+            // when & then
+            mockMvc.perform(get("/api/v1/patient"))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.name").value(response.getName()))
+                    .andExpect(jsonPath("$.residentRegistrationNumber").value(response.getResidentRegistrationNumber()))
+                    .andExpect(jsonPath("$.phoneNumber").value(response.getPhoneNumber()))
+                    .andExpect(jsonPath("$.address.street").value(response.getAddress().getStreet()));
+        }
+
+        @Test
+        @DisplayName("환자 프로필을 조회할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserPatient
+        void failGetPatientProfileNotFound() throws Exception {
+            // given
+            given(patientService.getProfile(any(String.class))).willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST));
+
+            // when & then
+            mockMvc.perform(get("/api/v1/patient"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 
-    @Test
-    @DisplayName("환자 프로필을 성공적으로 조회한다.")
-    @WithCustomMockUserPatient
-    void successGetPatientProfile() throws Exception {
-        // given
-        PatientProfileResponse response = createPatientProfileResponse();
-        given(patientService.getProfile(any(String.class))).willReturn(response);
+    @Nested
+    class 환자_프로필_수정 {
 
-        // when & then
-        mockMvc.perform(get("/api/v1/patient"))
-                .andDo(print())
-                .andExpect(status().isOk());
+        @Test
+        @DisplayName("환자 프로필을 성공적으로 수정한다.")
+        @WithCustomMockUserPatient
+        void successUpdatePatientProfile() throws Exception {
+            // given
+            PatientProfileUpdateRequest request = updatePatientProfileRequest();
+            willDoNothing().given(patientService).updatePatientProfile(any(String.class), any(PatientProfileUpdateRequest.class));
+
+            // when & then
+            mockMvc.perform(patch("/api/v1/patient")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isNoContent());
+        }
+
+        @Test
+        @DisplayName("환자 프로필을 수정할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserPatient
+        void failUpdatePatientProfileNotFound() throws Exception {
+            // given
+            PatientProfileUpdateRequest request = updatePatientProfileRequest();
+            willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST)).given(patientService).updatePatientProfile(any(String.class), any(PatientProfileUpdateRequest.class));
+
+            // when & then
+            mockMvc.perform(patch("/api/v1/patient")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(request)))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 
-    @Test
-    @DisplayName("환자 프로필을 성공적으로 수정한다.")
-    @WithCustomMockUserPatient
-    void successUpdatePatientProfile() throws Exception {
-        // given
-        PatientProfileUpdateRequest request = updatePatientProfileRequest();
-        willDoNothing().given(patientService).updatePatientProfile(any(String.class), any(PatientProfileUpdateRequest.class));
+    @Nested
+    class 환자_프로필_삭제 {
 
-        // when & then
-        mockMvc.perform(patch("/api/v1/patient")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-    }
+        @Test
+        @DisplayName("환자 프로필을 성공적으로 삭제한다.")
+        @WithCustomMockUserPatient
+        void successDeletePatientProfile() throws Exception {
+            // given
+            willDoNothing().given(patientService).deletePatientProfile(any(String.class));
 
-    @Test
-    @DisplayName("환자 프로필을 성공적으로 삭제한다.")
-    @WithCustomMockUserPatient
-    void successDeletePatientProfile() throws Exception {
-        // given
-        willDoNothing().given(patientService).deletePatientProfile(any(String.class));
+            // when & then
+            mockMvc.perform(delete("/api/v1/patient"))
+                    .andDo(print())
+                    .andExpect(status().isNoContent());
+        }
 
-        // when & then
-        mockMvc.perform(delete("/api/v1/patient"))
-                .andDo(print())
-                .andExpect(status().isNoContent());
-    }
+        @Test
+        @DisplayName("환자 프로필을 삭제할 때 프로필이 없으면 예외가 발생한다.")
+        @WithCustomMockUserPatient
+        void failDeletePatientProfileNotFound() throws Exception {
+            // given
+            willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST)).given(patientService).deletePatientProfile(any(String.class));
 
-    @Test
-    @DisplayName("환자 프로필을 등록할 때 잘못된 요청이면 예외가 발생한다.")
-    @WithCustomMockUserPatient
-    void failCreatePatientProfileBadRequest() throws Exception {
-        // given
-        PatientProfileCreateRequest request = new PatientProfileCreateRequest("", "", "", null, "", "", "", "");
-
-        // when & then
-        mockMvc.perform(post("/api/v1/patient")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    @DisplayName("환자 프로필을 조회할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserPatient
-    void failGetPatientProfileNotFound() throws Exception {
-        // given
-        given(patientService.getProfile(any(String.class))).willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST));
-
-        // when & then
-        mockMvc.perform(get("/api/v1/patient"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("환자 프로필을 수정할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserPatient
-    void failUpdatePatientProfileNotFound() throws Exception {
-        // given
-        PatientProfileUpdateRequest request = updatePatientProfileRequest();
-        willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST)).given(patientService).updatePatientProfile(any(String.class), any(PatientProfileUpdateRequest.class));
-
-        // when & then
-        mockMvc.perform(patch("/api/v1/patient")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-    }
-
-    @Test
-    @DisplayName("환자 프로필을 삭제할 때 프로필이 없으면 예외가 발생한다.")
-    @WithCustomMockUserPatient
-    void failDeletePatientProfileNotFound() throws Exception {
-        // given
-        willThrow(new EntityNotFoundException(ErrorCode.PATIENT_NOT_EXIST)).given(patientService).deletePatientProfile(any(String.class));
-
-        // when & then
-        mockMvc.perform(delete("/api/v1/patient"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
+            // when & then
+            mockMvc.perform(delete("/api/v1/patient"))
+                    .andDo(print())
+                    .andExpect(status().isNotFound());
+        }
     }
 }
