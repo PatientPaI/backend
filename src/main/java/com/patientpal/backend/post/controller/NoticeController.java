38f415cbfac4cc5898caae4185729bf0cb98d6ab
wjdwwidz <wjdwwidz@gmail.com>
2024-06-10 16:34:11 +0900

    Refactor: 공지사항 엔드포인트 변경

--- src/main/java/com/patientpal/backend/post/controller/NoticeController.java
+++ src/main/java/com/patientpal/backend/post/controller/NoticeController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/posts")
+@RequestMapping("/api/v1/notice")
 @RequiredArgsConstructor
 public class NoticeController {
 
