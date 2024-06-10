410c1c503a1ae6295aa1a73144371c2799205f9e
su6net <su6net@gmail.com>
2024-06-11 04:42:24 +0900

    Fix: 리프레시 토큰이 아니라 액세스 토큰을 받도록 수정함

--- src/main/java/com/patientpal/backend/auth/dto/AccessTokenResponse.java
+++ src/main/java/com/patientpal/backend/auth/dto/AccessTokenResponse.java
@@ -2,5 +2,5 @@ package com.patientpal.backend.auth.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record AccessTokenResponse(@JsonProperty("refresh_token") String refreshToken) {
+public record AccessTokenResponse(@JsonProperty("access_token") String accessToken) {
 }
