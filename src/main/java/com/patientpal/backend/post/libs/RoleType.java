819367cf96aa9f9c83f78586cb88c69618bfcfa9
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-06-20 11:38:08 +0900

    Feat: @RoleType 어노테이션 반복 사용 기능 추가 (#98)
    * Feat: RoleType 어노테이션 생성
    
    * Feat: Patient Role 추가
    
    * Feat: RoleTypeInterceptor 생성
    
    * Feat: Create InterceptorConfig
    
    * Feat: RoleType annotation 적용
    
    * Feat: Update, Delete 메서드에 RoleType 어노테이션 적용
    
    * Delete: PATIENT 타입 제거
    
    * Feat: RoleTypes Interface 추가
    
    * Feat: RoleType 어노테이션에 @Repeatable 추가

--- src/main/java/com/patientpal/backend/post/libs/RoleType.java
+++ src/main/java/com/patientpal/backend/post/libs/RoleType.java
@@ -2,14 +2,12 @@ package com.patientpal.backend.post.libs;
 
 
 import com.patientpal.backend.member.domain.Role;
-
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Target(ElementType.METHOD)
+@Repeatable(value = RoleTypes.class)
 @Retention(RetentionPolicy.RUNTIME)
+
 public @interface RoleType {
     // ADMIN, USER, CAREGIVER
     Role value();
