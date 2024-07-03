c472a80892b526938a44ad12b92df6311bbe4b94
wjdwwidz <wjdwwidz@gmail.com>
2024-07-03 16:35:01 +0900

    Feat: MessageRepository 추가

---
+++ src/main/java/com/patientpal/backend/chat/repository/MessageRepository.java
@@ -0,0 +1,11 @@
+package com.patientpal.backend.chat.repository;
+
+import com.patientpal.backend.chat.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, String> {
+
+
+}
