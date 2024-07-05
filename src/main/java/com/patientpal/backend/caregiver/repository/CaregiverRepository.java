29856412d5e964461e68698249424cebd1c713bf
LeeHuSung <mn040820@naver.com>
2024-07-05 16:32:35 +0900

    Feat: 간병인 관련 프로필 생성 시 원하는 간병 시작 날짜, 종료 날짜 추가

--- src/main/java/com/patientpal/backend/caregiver/repository/CaregiverRepository.java
+++ src/main/java/com/patientpal/backend/caregiver/repository/CaregiverRepository.java
@@ -1,7 +1,10 @@
 package com.patientpal.backend.caregiver.repository;
 
 import com.patientpal.backend.caregiver.domain.Caregiver;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CaregiverRepository extends JpaRepository<Caregiver, Long>, CaregiverProfileSearchRepositoryCustom {
+
+    Optional<Caregiver> findByUsername(String username);
 }
