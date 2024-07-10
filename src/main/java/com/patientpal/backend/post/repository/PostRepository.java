efaad6002d10dabe2cde3c15ff92fd12f64993cf
wjdwwidz <wjdwwidz@gmail.com>
2024-07-10 17:37:41 +0900

    Feat: Page type을 반환하는 findAll 메서드 추가

--- src/main/java/com/patientpal/backend/post/repository/PostRepository.java
+++ src/main/java/com/patientpal/backend/post/repository/PostRepository.java
@@ -2,15 +2,18 @@ package com.patientpal.backend.post.repository;
 
 import com.patientpal.backend.post.domain.Post;
 import com.patientpal.backend.post.domain.PostType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByPostType(PostType postType);
 
     Optional<Post> findByIdAndMemberId(long postId, long memberId);
+
+    Page<Post> findAll(Pageable pageable);
 
 }
