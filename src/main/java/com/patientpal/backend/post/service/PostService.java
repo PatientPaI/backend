c891c7ff45bd326342e710b5d94fdd7a257007cd
wjdwwidz <wjdwwidz@gmail.com>
2024-07-14 21:53:56 +0900

    Feat: Pageable 객체 반환하는 getList 메서드 추가

--- src/main/java/com/patientpal/backend/post/service/PostService.java
+++ src/main/java/com/patientpal/backend/post/service/PostService.java
@@ -2,25 +2,36 @@ package com.patientpal.backend.post.service;
 
 import com.patientpal.backend.common.exception.EntityNotFoundException;
 import com.patientpal.backend.common.exception.ErrorCode;
-import com.patientpal.backend.common.page.domain.Paginator;
 import com.patientpal.backend.member.domain.Member;
 import com.patientpal.backend.post.domain.Post;
 import com.patientpal.backend.post.domain.PostType;
 import com.patientpal.backend.post.dto.PostCreateRequest;
 import com.patientpal.backend.post.dto.PostUpdateRequest;
 import com.patientpal.backend.post.repository.PostRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PostService {
 
     private final PostRepository postRepository;
+
+    public Page <Post> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createdDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        // page : 조회할 페이지의 번호
+        // 10 : 한 페이지에 보여 줄 게시물의 개수
+        return this.postRepository.findAll(pageable);
+    }
 
     // TODO: wjdwwidz paging 처리
     @Transactional(readOnly = true)
@@ -51,20 +62,19 @@ public class PostService {
     }
 
     @Transactional
-    public Post updatePost(Member member, Long id, PostUpdateRequest updateRequest) { 
+    public Post updatePost(Member member, Long id, PostUpdateRequest updateRequest) {
         Post post = postRepository.findByIdAndMemberId(id, member.getId())
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.POST_NOT_FOUND));
         post.update(updateRequest.getTitle(), updateRequest.getContent());
         return post;
     }
 
-    public void deletePost(Member member, Long id) { 
+    public void deletePost(Member member, Long id) {
         postRepository.findByIdAndMemberId(id, member.getId())
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.POST_NOT_FOUND));
 
         postRepository.deleteById(id);
     }
-
 
 
     @Transactional
