1f672ce8840eab3c063d9f7273122efa7c80810c
wjdwwidz <wjdwwidz@gmail.com>
2024-07-15 15:34:48 +0900

    Feat: findAllByPostType메서드에 PostType 매개변수 추가

--- src/main/java/com/patientpal/backend/post/repository/PostRepository.java
+++ src/main/java/com/patientpal/backend/post/repository/PostRepository.java
@@ -2,7 +2,6 @@ package com.patientpal.backend.post.repository;
 
 import com.patientpal.backend.post.domain.Post;
 import com.patientpal.backend.post.domain.PostType;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    List<Post> findAllByPostType(PostType postType);
+    Page<Post> findAllByPostType(PostType postType, Pageable pageable);
 
     Optional<Post> findByIdAndMemberId(long postId, long memberId);
 
