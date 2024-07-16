62bf1ef661bcfc965413f366fbe9027c0895f43c
LeeHuSung <mn040820@naver.com>
2024-07-16 16:06:14 +0900

    Feat: 검색을 위한 공통 설정 추가

--- src/main/java/com/patientpal/backend/common/BaseTimeEntity.java
+++ src/main/java/com/patientpal/backend/common/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -24,4 +25,7 @@ public abstract class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    @Setter
+    private LocalDateTime profilePublicTime;
 }
