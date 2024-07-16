c5c7e74b996270e4a01ed4345830a2cebf0aa179
LeeHuSung <mn040820@naver.com>
2024-07-16 17:09:51 +0900

    Feat: fix register admin

--- src/main/java/com/patientpal/backend/member/service/MemberService.java
+++ src/main/java/com/patientpal/backend/member/service/MemberService.java
@@ -58,7 +58,7 @@ public class MemberService {
                         .build();
                 patient.encodePassword(passwordEncoder);
                 return patientRepository.save(patient).getId();
-            } else {
+            } else if (request.getRole() == Role.ADMIN) {
                 Member member = Member.builder()
                         .username(request.getUsername())
                         .password(request.getPassword())
@@ -67,6 +67,8 @@ public class MemberService {
                         .build();
                 return memberRepository.save(member).getId();
             }
+            throw new BusinessException(ErrorCode.UNSELECTED_ROLE);
+
         } catch (DataIntegrityViolationException e) {
             throw new AuthenticationException(ErrorCode.MEMBER_ALREADY_EXIST, request.getUsername());
         }
@@ -93,7 +95,7 @@ public class MemberService {
                         .provider(Provider.valueOf(request.getProvider().toUpperCase()))
                         .build();
                 return patientRepository.save(patient).getId();
-            } else {
+            } else if (request.getRole() == Role.ADMIN) {
                 Member member = Member.builder()
                         .username(request.getUsername())
                         .password(request.getPassword())
@@ -102,6 +104,8 @@ public class MemberService {
                         .build();
                 return memberRepository.save(member).getId();
             }
+            throw new BusinessException(ErrorCode.UNSELECTED_ROLE);
+
         } catch (DataIntegrityViolationException e) {
             throw new AuthenticationException(ErrorCode.MEMBER_ALREADY_EXIST, request.getEmail());
         }
