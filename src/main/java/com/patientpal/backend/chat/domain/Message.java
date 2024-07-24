2c2e165c19da2407e0ce48dab75d5ca254d4a48c
wjdwwidz <wjdwwidz@gmail.com>
2024-07-25 02:06:01 +0900

    Feat: Create Message Entity

--- src/main/java/com/patientpal/backend/chat/domain/Message.java
+++ src/main/java/com/patientpal/backend/chat/domain/Message.java
@@ -1,27 +1,34 @@
 package com.patientpal.backend.chat.domain;
 
+import com.patientpal.backend.chat.dto.MessageType;
 import com.patientpal.backend.common.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
 @Entity
 public class Message extends BaseTimeEntity {
 
-    //컬럼추가
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private MessageType messageType;
 
     private String content;
 
     private Long senderId;
 
-    private Long receiverId;
+    private Long chatId;
 }
