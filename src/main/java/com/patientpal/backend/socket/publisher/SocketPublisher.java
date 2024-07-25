e47c973ed7d89251b4845d89281606fa0f69583b
wjdwwidz <wjdwwidz@gmail.com>
2024-07-26 01:33:34 +0900

    Feat: SocketPublisher의 동작을 async하게 변경

--- src/main/java/com/patientpal/backend/socket/publisher/SocketPublisher.java
+++ src/main/java/com/patientpal/backend/socket/publisher/SocketPublisher.java
@@ -4,6 +4,7 @@ import com.patientpal.backend.chat.dto.SocketDirectMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -13,6 +14,7 @@ public class SocketPublisher {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
 
+    @Async
     public void sendMessage(Long chatId, SocketDirectMessage message) {
         log.info("send message. chatId: "+ chatId + " content: "+ message.getContent());
         simpMessagingTemplate.convertAndSend("/topic/directChat/" + chatId, message);
