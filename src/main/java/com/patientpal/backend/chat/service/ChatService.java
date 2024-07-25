2adfac2012a554f0d8f543d1baee25d67513ad4b
wjdwwidz <wjdwwidz@gmail.com>
2024-07-26 01:29:26 +0900

    Feat: ChatService에 create 메서드 추가

--- src/main/java/com/patientpal/backend/chat/service/ChatService.java
+++ src/main/java/com/patientpal/backend/chat/service/ChatService.java
@@ -1,11 +1,13 @@
 package com.patientpal.backend.chat.service;
 
 import com.patientpal.backend.chat.domain.Chat;
+import com.patientpal.backend.chat.dto.ChatCreateRequest;
 import com.patientpal.backend.chat.repository.ChatRepository;
 import com.patientpal.backend.common.exception.EntityNotFoundException;
 import com.patientpal.backend.common.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,15 +15,19 @@ public class ChatService {
 
     private final ChatRepository chatRepository;
 
-
+    @Transactional(readOnly = true)
     public Chat getChat(Long chatId) {
         return findChat(chatId);
     }
 
-
     private Chat findChat(Long chatId) {
         return chatRepository.findById(chatId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorCode.CHAT_NOT_FOUND));
+    }
+
+    @Transactional
+    public Chat create(ChatCreateRequest request) {
+        return chatRepository.save(request.toEntity());
     }
 }
 
