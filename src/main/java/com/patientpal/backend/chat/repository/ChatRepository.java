1288cb623832bb5fb4541c066484368a762a0bef
wjdwwidz <wjdwwidz@gmail.com>
2024-07-28 20:21:08 +0900

    Fix: MariaDB 용 쿼리 수정

--- src/main/java/com/patientpal/backend/chat/repository/ChatRepository.java
+++ src/main/java/com/patientpal/backend/chat/repository/ChatRepository.java
@@ -1,13 +1,14 @@
 package com.patientpal.backend.chat.repository;
 
 import com.patientpal.backend.chat.domain.Chat;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    @Query(value = "SELECT * FROM Chat c WHERE JSON_CONTAINS(c.memberIds, :memberId, '$')", nativeQuery = true)
+    @Query(value = "SELECT * FROM Chat c WHERE JSON_CONTAINS(c.memberIds, CAST(:memberId AS JSON), '$')", nativeQuery = true)
     List<Chat> findAllByMemberId(@Param("memberId") Long memberId);
 }
