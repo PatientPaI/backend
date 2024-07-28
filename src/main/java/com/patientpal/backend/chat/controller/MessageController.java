82312e2d7884820aba45633083c55d113aeb0161
wjdwwidz <wjdwwidz@gmail.com>
2024-07-28 19:15:36 +0900

    Fix: User 권한검사 추가

--- src/main/java/com/patientpal/backend/chat/controller/MessageController.java
+++ src/main/java/com/patientpal/backend/chat/controller/MessageController.java
@@ -4,6 +4,8 @@ import com.patientpal.backend.chat.dto.MessageCreateRequest;
 import com.patientpal.backend.chat.dto.MessageCreateResponse;
 import com.patientpal.backend.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,7 @@ public class MessageController {
     private final MessageService messageService;
 
     @PostMapping
-    public MessageCreateResponse create(@RequestBody MessageCreateRequest request) {
+    public MessageCreateResponse create(@RequestBody MessageCreateRequest request, @AuthenticationPrincipal UserDetails user) {
         var message = messageService.createAndSendMessage(request);
         return new MessageCreateResponse(message);
     }
