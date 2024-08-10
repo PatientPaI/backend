3395187eddd9fb8e1a71d4739100a34e6ca08dd2
wjdwwidz <wjdwwidz@gmail.com>
2024-08-11 01:20:00 +0900

    feat: Implements paging in Message

--- src/main/java/com/patientpal/backend/chat/controller/MessageController.java
+++ src/main/java/com/patientpal/backend/chat/controller/MessageController.java
@@ -1,11 +1,15 @@
 package com.patientpal.backend.chat.controller;
 
+import com.patientpal.backend.chat.domain.Message;
 import com.patientpal.backend.chat.dto.MessageCreateRequest;
 import com.patientpal.backend.chat.dto.MessageCreateResponse;
+import com.patientpal.backend.chat.dto.MessageRequestParam;
 import com.patientpal.backend.chat.service.MessageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +26,10 @@ public class MessageController {
     public MessageCreateResponse create(@RequestBody MessageCreateRequest request, @AuthenticationPrincipal UserDetails user) {
         var message = messageService.createAndSendMessage(request);
         return new MessageCreateResponse(message);
+    }
+
+    @GetMapping
+    public Page<Message> list(MessageRequestParam param, @AuthenticationPrincipal UserDetails user) {
+        return messageService.getMessages(param);
     }
 }
--- src/main/java/com/patientpal/backend/chat/repository/MessageRepository.java
+++ src/main/java/com/patientpal/backend/chat/repository/MessageRepository.java
@@ -1,9 +1,13 @@
 package com.patientpal.backend.chat.repository;
 
 import com.patientpal.backend.chat.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findAllByChatId(Long id, Pageable pageable);
 }
