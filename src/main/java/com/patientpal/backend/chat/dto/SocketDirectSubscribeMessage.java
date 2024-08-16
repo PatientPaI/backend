a9ffca229c18e58510941ca8e72b9f73ae726515
JeongHye <88189246+wjdwwidz@users.noreply.github.com>
2024-08-16 11:27:40 +0900

    Feat: Add socket message attributes (#228)
    * Feat: Add socket message attributes
    
    * Feat: Add userName in SocketDirectSubscribeMessage

--- src/main/java/com/patientpal/backend/chat/dto/SocketDirectSubscribeMessage.java
+++ src/main/java/com/patientpal/backend/chat/dto/SocketDirectSubscribeMessage.java
@@ -3,6 +3,7 @@ package com.patientpal.backend.chat.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,5 +12,9 @@ public class SocketDirectSubscribeMessage {
 
     private String content;
     private Long senderId;
+    private String userName;
+    private String name;
+    private String profileImageUrl;
+    private LocalDateTime createdAt;
     private MessageType messageType;
 }
